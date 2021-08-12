889c1fbaf2b08affde43139555cb07088b235593
unknown <fizz.zehra14@gmail.com>
2021-08-12 16:23:14 -0500

    COMMENTS

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,17 +1,17 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        comment_text: "Lorem ipsum dolor sit amet",
+        comment_text: "WOW! THIS IS AMAZING MAN",
         user_id: 1,
         post_id: 1
     },
     {
-        comment_text: "consectetur adipiscing elit",
+        comment_text: "IMPRESSIVE",
         user_id: 2,
         post_id: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        comment_text: "GREAT WORK!",
         user_id: 3,
         post_id: 3
     }
