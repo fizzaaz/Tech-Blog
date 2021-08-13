fb1f97429d9d8f0f95472aca4f40b74975d85a91
unknown <fizz.zehra14@gmail.com>
2021-08-12 19:56:45 -0500

    make it better

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,7 +4,6 @@ const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-    console.log('======================');
     Post.findAll({
             attributes: ['id',
                 'title',
