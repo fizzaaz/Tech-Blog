c2c10457136f867918c701c28bb3bb9877850933
unknown <fizz.zehra14@gmail.com>
2021-08-12 20:03:24 -0500

    make it better

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-const withAuth = require('../../utils');
+const withAuth = require('../../utils/auth');
 router.get('/', (req, res) => {
     Comment.findAll({})
         .then(dbCommentData => res.json(dbCommentData))
